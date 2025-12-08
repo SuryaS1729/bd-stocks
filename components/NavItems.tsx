c3f8afb97469b3baae4ba86f84f58aa2122760b3
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-12-09 00:27:21 +0530

    redid constants

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import NAV_ITEMS from '@/lib/constants'
+
+import { NAV_ITEMS } from '@/lib/constants'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
@@ -19,9 +20,9 @@ export default function NavItems({}: Props) {
     }
   return (
     <div className='flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium '>
-        {NAV_ITEMS.map(({href,name})=>(
+        {NAV_ITEMS.map(({href,label}:any)=>(
             <ul key={href}>
-                <Link href={href} className={`hover:text-yellow-500 transition-colors ${isActive(href)? 'text-gray-200':''}`}>{name}
+                <Link href={href} className={`hover:text-yellow-500 transition-colors ${isActive(href)? 'text-gray-200':''}`}>{label}
                 </Link>
             </ul>
         ))}
