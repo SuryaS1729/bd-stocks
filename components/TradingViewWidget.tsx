50772f92d9ac7f4aae16e7db66308b0f485ba8ad
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-12-19 04:43:41 +0530

    beginning charts

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -20,8 +20,7 @@ const TradingViewWidget = ({title, scriptUrl, config, height=600, className}:Tra
     <div className='w-full'>
       {title && <h2 className='mb-5 text-2xl font-semibold text-gray-100'>{title}</h2>}
     <div className={cn("tradingview-widget-container", className)} ref={containerRef}>
-      <div className="tradingview-widget-container__widget" style={{ height: "calc(100% - 32px)", width: "100%" }}></div>
-      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener nofollow" target="_blank"><span className="blue-text">AAPL stock chart</span></a><span className="trademark"> by TradingView</span></div>
+      <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }}/>
     </div>
     </div>
   );
