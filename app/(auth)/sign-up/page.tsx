da00bf1c498d0187ed7b93da9fdd97105a3ef26a
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-12-20 01:51:58 +0530

    added first form buttons

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 import { useForm, SubmitHandler } from "react-hook-form"
 
@@ -14,13 +16,34 @@ export default function SignUp({}: Props) {
     handleSubmit,
     control,
     formState: { errors, isSubmitting },
-  } = useForm<SignUpFormData>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
-    
+  } = useForm<SignUpFormData>({
+    defaultValues:{
+    fullName: '',
+    email: '',
+    password: '',
+    country: "India",
+    investmentGoals:'Growth',
+    riskTolerance:'Medium',
+    preferredIndustry:'Technology'
+  }, mode: 'onBlur'
+  })
+  const onSubmit = async(data:SignUpFormData) => {
+try {
+    console.log('Sign up data:', data);
+} catch (error) {
+    console.log('Sign up error:', error);
+}
+  }
+
   return (
 
 <>
 <h1 className='form-title'>Sign Up & Personalize </h1>
+<form onSubmit={handleSubmit(onSubmit)} className='space-y-5'>
+<Button type="submit" disabled={isSubmitting} className='yellow-btn w-full mt-5'>
+    {isSubmitting ? 'Creating Account...' : 'Start Your Investing Journey'}
+</Button>
+</form>
 
 
 
