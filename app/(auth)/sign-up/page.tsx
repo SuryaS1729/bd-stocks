d6a3fba5f1f4b1652d32774d492f3b673128b95e
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-12-20 14:03:58 +0530

    designed select field

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import InputField from '@/components/forms/InputField'
+import SelectField from '@/components/forms/SelectField'
 import { Button } from '@/components/ui/button'
+import { INVESTMENT_GOALS } from '@/lib/constants'
 import React from 'react'
 import { useForm, SubmitHandler } from "react-hook-form"
 
@@ -50,6 +52,21 @@ try {
     validation={{ required: "Full name is required", minLength: { value: 2, message: "Full name must be at least 2 characters long" } }} />
     <InputField name="email" label="Email Address" placeholder="Enter your email address" register={register} error={errors.email} validation={{ required: "Email is required", pattern: { value: /^\S+@\S+$/i, message: "Invalid email address" } }} />
     <InputField name="password" label="Password" type="password" placeholder="Create a password" register={register} error={errors.password} validation={{ required: "Password is required", minLength: { value: 8, message: "Password must be at least 6 characters long" } }} />
+    
+    <SelectField
+     name="investmentGoals  "   
+        label="Investment Goals"
+        placeholder="Select your investment goals"
+        options = {INVESTMENT_GOALS}
+        control ={control}
+        error={errors.investmentGoals}
+        
+    />
+
+    
+    
+    
+    
     <Button type="submit" disabled={isSubmitting} className='yellow-btn w-full mt-5'>
         {isSubmitting ? 'Creating Account...' : 'Start Your Investing Journey'}
     </Button>
