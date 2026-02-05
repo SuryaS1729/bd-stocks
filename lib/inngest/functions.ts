38d0fb0c79433a2640ee805173fe266f79310c57
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2026-02-05 19:53:12 +0530

    connected to inngest local dev server

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -1,4 +1,5 @@
 import { inngest } from "./client";
+import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "./prompts";
 
 
 export const sendSignupEmail = inngest.createFunction(
@@ -12,6 +13,39 @@ export const sendSignupEmail = inngest.createFunction(
         - Preferred Industry:${event.data.investmentGoals}
 
         `
+
+        const prompt =PERSONALIZED_WELCOME_EMAIL_PROMPT.replace('{{userProfile}}',userProfile)
+
+        const response = await step.ai.infer("generate-welcome-intro",{
+            model:step.ai.models.gemini({model:'gemini-2.5-flash-lite'}),
+            body:{
+                    contents:[
+                        {
+                            role:'user',
+                            parts:[
+                                {
+                                    text:prompt 
+                                }
+                            ]
+                        }
+                    ]
+                }
+            
+        })
+
+        await step.run('send-welcome-email', async ()=>{
+            const part = response.candidates?.[0]?.content?.parts?.[0]
+            
+            const introText = (part && 'text' in part ? part.text : null) || "Thanks for joining BitwiseStocks. You now have the tools to track markets and make smarter decisions"
+       
+       //email sending logic
+       
+        })
+
+        return {
+            success: true,
+            message: "Welcome Email sent successfully"
+        }
 
     }
 )
